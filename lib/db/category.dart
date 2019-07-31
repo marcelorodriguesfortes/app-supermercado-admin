9f787afd44e5e74570c4107b4f1646150e40d4ea
Marcelo <marcelorodrigues000@gmail.com>
2019-07-31 20:15:53 -0300

    Continuação da funcionalidade de adição de produtos ao firebase

--- lib/db/category.dart
+++ lib/db/category.dart
@@ -3,10 +3,17 @@ import 'package:uuid/uuid.dart';
 
 class CategoryService{
   Firestore _firestore = Firestore.instance;
+  String ref = 'categories';
 
   void createCategory(String name){
   var id = Uuid();
   String categoryId = id.v1();
     _firestore.collection('categories').document(categoryId).setData({'category': name});
+  }
+
+  Future<List<DocumentSnapshot>> getCategories(){
+    return _firestore.collection(ref).getDocuments().then((snaps){
+      return snaps.documents;
+    });
   }
 }
--- lib/screens/add_products.dart
+++ lib/screens/add_products.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-import 'dart:convert';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../db/category.dart';
+import '../db/brand.dart';
+
 
 class AddProducts extends StatefulWidget {
   @override
@@ -12,6 +14,37 @@ class _AddProductsState extends State<AddProducts> {
   Color black = Colors.black;
   Color blue = Colors.blue;
   Color grey = Colors.grey;
+  CategoryService _categoryService = CategoryService();
+  BrandService _brandService = BrandService();
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  TextEditingController productNameController = TextEditingController();
+  List<DocumentSnapshot> brands = <DocumentSnapshot>[];
+  List<DocumentSnapshot> categories = <DocumentSnapshot>[];
+  List<DropdownMenuItem<String>> categoriesDropDown = <DropdownMenuItem<String>>[];
+  List<DropdownMenuItem<String>> brandsDropDown = <DropdownMenuItem<String>>[];
+  String _currentCategory = "test";
+  String _currentBrand;
+
+
+
+  @override
+  void initState() {
+    _getCategories();
+    //_getBrands();
+    getCategoriesDropdown();
+    //_currentCategory = categoriesDropDown[0].value;
+  }
+
+  getCategoriesDropdown(){
+    List<DropdownMenuItem<String>> items = new List();
+    for (int i = 0; i < categories.length; i++){
+      categoriesDropDown.insert(0, DropdownMenuItem(
+        child: Text(categories[i]['category']),
+        value: categories[i]['category'],
+      )
+      );
+    }
+  }
   
   @override
   Widget build(BuildContext context) {
@@ -22,55 +55,112 @@ class _AddProductsState extends State<AddProducts> {
         leading: Icon(Icons.close, color: black,),
         title: Text("add products", style: TextStyle(color: black),),
       ),
-      body: ListView(
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: OutlineButton(
-                    borderSide: BorderSide(color: grey.withOpacity(0.8), width: 2.0),
-                    onPressed:(){},
-                    child: Padding(
-                      padding: const EdgeInsets.fromLTRB(14.0, 30.0, 14.0, 30.0),
-                      child: new Icon(Icons.add, color: grey,),
+      body: Form(
+          key: _formKey,
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: OutlineButton(
+                      borderSide: BorderSide(color: grey.withOpacity(0.8), width: 2.5),
+                      onPressed:(){},
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(14.0, 30.0, 14.0, 30.0),
+                        child: new Icon(Icons.add, color: grey,),
+                      ),
                     ),
                   ),
                 ),
-              ),
 
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: OutlineButton(
-                    borderSide: BorderSide(color: grey.withOpacity(0.8), width: 2.0),
-                    onPressed:(){},
-                    child: Padding(
-                      padding: const EdgeInsets.fromLTRB(14.0, 30.0, 14.0, 30.0),
-                      child: new Icon(Icons.add, color: grey,),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: OutlineButton(
+                      borderSide: BorderSide(color: grey.withOpacity(0.8), width: 2.5),
+                      onPressed:(){},
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(14.0, 30.0, 14.0, 30.0),
+                        child: new Icon(Icons.add, color: grey,),
+                      ),
                     ),
                   ),
                 ),
-              ),
 
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: OutlineButton(
-                    borderSide: BorderSide(color: grey.withOpacity(0.8), width: 2.0),
-                    onPressed:(){},
-                    child: Padding(
-                      padding: const EdgeInsets.fromLTRB(14.0, 30.0, 14.0, 30.0),
-                      child: new Icon(Icons.add, color: grey,),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: OutlineButton(
+                      borderSide: BorderSide(color: grey.withOpacity(0.8), width: 2.5),
+                      onPressed:(){},
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(14.0, 30.0, 14.0, 30.0),
+                        child: new Icon(Icons.add, color: grey,),
+                      ),
                     ),
                   ),
                 ),
+              ],
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Text('Digite o nome do produto: ',
+              style: TextStyle(color: blue),
               ),
-            ],
-          )
-        ],
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: TextFormField(
+                controller: productNameController,
+                decoration: InputDecoration(
+                  hintText: "Nome do produto"
+                ),
+                validator: (value){
+                 if(value.isEmpty){
+                   return 'Digite um nome para o produto';
+                 }else if(value.length > 10){
+                   return 'O nome do produto não pode ultrapassar 10 caracteres';
+                 }
+                 return null;
+                },
+              ),
+            ),
+
+            Expanded(
+              child: ListView.builder(
+                  itemCount: categories.length,
+                  itemBuilder: (context, index){
+                    return ListTile(
+                      title: Text(categories[index]['category']),
+                    );
+                  }),
+            )
+            /*Center(
+              child: DropdownButton(
+                  value: _currentCategory,
+                  items: categoriesDropDown,
+                  onChanged: changeSelectedCategory),
+            )*/
+          ],
+        ),
       ),
     );
+  }
+
+   _getCategories() async {
+    List<DocumentSnapshot> data = await _categoryService.getCategories();
+    setState(() {
+      categories = data;
+    });
+   }
+
+  changeSelectedCategory(String selectedCategory) {
+      setState(() {
+        _currentCategory = selectedCategory;
+      });
   }
 }
